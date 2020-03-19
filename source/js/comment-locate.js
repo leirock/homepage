eebb3b72d3fe49efbf0433107a9ae1bc60ced857
leirock <19180725+leirock@users.noreply.github.com>
2020-03-19 19:18:00 +0800

    Update COS CDN

--- source/js/comment-locate.js
+++
@@ -1,8 +0,0 @@
-if(window.location.hash){
-  var checkExist = setInterval(function() {
-    if ($(window.location.hash).length) {
-      $('html, body').animate({scrollTop: $(window.location.hash).offset().top-90}, 1000);
-      clearInterval(checkExist);
-    }
-  }, 100);
-}
