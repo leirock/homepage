6931a41eddfbc3c0176be65d8f3217bfbbc40b63
leirock <19180725+leirock@users.noreply.github.com>
2020-04-04 15:02:36 +0800

    Optimize hexo script & styles

--- hexo.sh
+++ hexo.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
-echo "=========================================================================================="
-echo "=========================================================================================="
+echo "========================================================================================"
+echo "                                    Hexo Website Panel"
 echo " "
-echo "                                      Hexo Website Panel"
-echo " "
-echo "                                         by @lei2rock"
-echo " "
-echo "=========================================================================================="
-echo "=========================================================================================="
+echo "                                       by @lei2rock"
+echo "========================================================================================"
 HexoPath=$(cd "$(dirname "$0")"; pwd)
 cd ${HexoPath}
-printf "\033[32mINFO \033[0m Hexo 根目录："${HexoPath};
+echo " "
+printf "\033[32m Homepage 根目录："${HexoPath}"\033[0m";
 echo " "
 echo " "
+echo "[0] 切换到 Blog";
 echo "[1] 本地预览";
 echo "[2] 更新主题";
 echo "[3] 新主题提交 GitHub";
+echo "[4] 复制 Blog 样式";
 echo " "
-printf "请选择：";
+printf "请选择需要的功能，默认选择[1]";
+echo " "
+echo " "
+printf "选择：";
 read answer
 if [ "$answer" == "1" ] || [ "$answer" == "" ]; then
 	echo " "
@@ -33,6 +35,7 @@ if [ "$answer" == "1" ] || [ "$answer" == "" ]; then
 	sed -i "" 's#js: js#js: https://web-1256060851.file.myqcloud.com/assets/js#g' _config.yml
 	sed -i "" '18s/.replace(\/\!\[0-9\]{3,}x\/,\"\")//' themes/next/source/js/utils.js
 	sed -i "" '4s/"Posts | " + //' themes/next/layout/index.swig
+	echo " "
 	exec ${HexoPath}/hexo.sh
 else
 	if [ "$answer" == "2" ]; then
@@ -42,22 +45,59 @@ else
 		git submodule update --remote
 		echo " "
 		printf "\033[32mINFO \033[0m 主题更新完毕！\n";
+		sleep 1s;
+		echo " "
 		exec ${HexoPath}/hexo.sh
 	else
 		if [ "$answer" == "3" ]; then
 			echo " "
-			printf "\033[32mINFO \033[0m 准备提交 GitHub...\n";
+			printf "\033[32mINFO \033[0m 准备提交 GitHub ...\n";
 			echo " "
 			git add .
 			git commit -m "Update next"
 			git push origin src
 			echo " "
 			printf "\033[32mINFO \033[0m 提交 GitHub 完毕！\n";
+			sleep 1s;
+			echo " "
 			exec ${HexoPath}/hexo.sh
 		else
-			echo " "
-			printf "\033[31mERROR \033[0m 输入错误，请返回重新选择...\n";
-			exec ${HexoPath}/hexo.sh
+			if [ "$answer" == "4" ]; then
+				echo " "
+				printf "\033[32mINFO \033[0m 开始复制文件...\n";
+				echo "--------------------------------------------------"
+				cp -f ../blog/source/_data/body-end.swig source/_data/body-end.swig
+				printf "\033[32mINFO \033[0m 已复制 body-end.swig \n";
+				cp -f ../blog/source/_data/head.swig source/_data/head.swig
+				printf "\033[32mINFO \033[0m 已复制 head.swig \n";
+				cp -f ../blog/source/_data/mixins.styl source/_data/mixins.styl
+				printf "\033[32mINFO \033[0m 已复制 mixins.styl \n";
+				cp -f ../blog/source/_data/post-meta.swig source/_data/post-meta.swig
+				printf "\033[32mINFO \033[0m 已复制 post-mata.swig \n";
+				cp -f ../blog/source/_data/styles.styl source/_data/styles.styl
+				printf "\033[32mINFO \033[0m 已复制 styles.styl \n";
+				cp -f ../blog/source/_data/variables.styl source/_data/variables.styl
+				printf "\033[32mINFO \033[0m 已复制 variables.styl \n";
+				echo "--------------------------------------------------"
+				printf "\033[32mINFO \033[0m 文件复制完毕！\n";
+				sleep 1s;
+				echo " "
+				exec ${HexoPath}/hexo.sh
+			else
+				if [ "$answer" == "0" ]; then
+					echo " "
+					printf "\033[32mINFO \033[0m 准备切换到 Blog ...\n";
+					sleep 1s;
+					echo " "
+					exec ${HexoPath}/../blog/hexo.sh
+				else
+					echo " "
+					printf "\033[31mERROR \033[0m 输入错误，请返回重新选择...\n";
+					sleep 1s;
+					echo " "
+					exec ${HexoPath}/hexo.sh
+				fi
+			fi
 		fi
 	fi
 fi
